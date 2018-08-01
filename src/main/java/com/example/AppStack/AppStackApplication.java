7e1a799e4eb9395be6fb7aaa18ec1d12a739f337
github username <github email address>
2018-07-31 20:50:05 -0400

    first commit

---
+++ src/main/java/com/example/AppStack/AppStackApplication.java
@@ -0,0 +1,12 @@
+package com.example.AppStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppStackApplication.class, args);
+	}
+}
